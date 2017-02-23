e73871b376e4c8b4760173fd1f8fca40ecef8dc1
sorashiro <sora95shiro@gmail.com>
2017-02-23 20:07:35 +0800

    v0.0.1

---
+++ app/src/main/java/com/sorashiro/sorashirotools/RegexTools.java
@@ -0,0 +1,49 @@
+package com.sorashiro.sorashirotools;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Created by GameKing on 2016/7/25.
+ */
+public class RegexTools {
+
+    /**
+     * 判断手机号码是否合法
+     * @param mobiles
+     * @return
+     */
+    public static boolean isMobile(String mobiles){
+
+        Pattern p = Pattern.compile("^((13[0-9])|(15[^4,\\D])|(18[0,5-9]))\\d{8}$");
+
+        Matcher m = p.matcher(mobiles);
+
+        return m.matches();
+
+    }
+
+    /**
+     * 判断邮编是否合法
+     * @param zipString
+     * @return
+     */
+    public static boolean isZip(String zipString){
+        String str = "^[1-9][0-9]{5}$";
+        return Pattern.compile(str).matcher(zipString).matches();
+    }
+
+    /**
+     * 判断邮箱是否合法
+     * @param email
+     * @return
+     */
+    public static boolean isEmail(String email){
+        if (null==email || "".equals(email)) return false;
+        //Pattern p = Pattern.compile("\\w+@(\\w+.)+[a-z]{2,3}"); //简单匹配
+        Pattern p =  Pattern.compile("\\w+([-+.]\\w+)*@\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*");//复杂匹配
+        Matcher m = p.matcher(email);
+        return m.matches();
+    }
+
+}
