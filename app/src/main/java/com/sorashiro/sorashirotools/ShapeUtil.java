77dab0c23a1fb84ab0fc41f4878159be605a6e2e
sorashiro <sora95shiro@gmail.com>
2017-05-02 09:15:29 +0800

    v1.0.2

--- app/src/main/java/com/sorashiro/sorashirotools/ShapeUtil.java
+++ app/src/main/java/com/sorashiro/sorashirotools/ShapeUtil.java
@@ -16,6 +16,72 @@ import android.graphics.drawable.shapes.Shape;
 
 public class ShapeUtil {
 
+    public static ShapeDrawable[] getArrows(int color) {
+        ShapeDrawable aUp = ShapeUtil.getArrowDrawable("up", color);
+        ShapeDrawable aDown = ShapeUtil.getArrowDrawable("down", color);
+        ShapeDrawable aLeft = ShapeUtil.getArrowDrawable("left", color);
+        ShapeDrawable aRight = ShapeUtil.getArrowDrawable("right", color);
+        ShapeDrawable[] shapeDrawables = new ShapeDrawable[4];
+        shapeDrawables[0] = aUp;
+        shapeDrawables[1] = aDown;
+        shapeDrawables[2] = aLeft;
+        shapeDrawables[3] = aRight;
+        return shapeDrawables;
+    }
+
+    public static ShapeDrawable getArrowDrawable(String orientation, int color) {
+        ShapeDrawable d = new ShapeDrawable(getArrowShape(orientation));
+        d.getPaint().setColor(color);
+        return d;
+    }
+
+    public static Shape getArrowShape(final String orientation) {
+        Shape shape = new Shape() {
+            float w;
+            float h;
+            Path path = new Path();
+
+            @Override
+            protected void onResize(float width, float height) {
+                this.w = width;
+                this.h = height;
+            }
+
+            @Override
+            public void draw(Canvas canvas, Paint paint) {
+                canvas.translate(w / 2, h / 2);
+                switch (orientation) {
+                    case "down":
+                        canvas.rotate(90);
+                        break;
+                    case "left":
+                        canvas.rotate(180);
+                        break;
+                    case "up":
+                        canvas.rotate(270);
+                        break;
+                    case "right":
+                        break;
+                }
+                //Right Orientation
+                path.reset();
+                float small = w < h ? w : h;
+                path.moveTo(-0.43f * small, -0.07f * small);
+                path.lineTo(-0.43f * small, 0.07f * small);
+                path.lineTo(0.12f * small, 0.07f * small);
+                path.lineTo(-0.17f * small, 0.38f * small);
+                path.lineTo(0.06f * small, 0.38f * small);
+                path.lineTo(0.43f * small, 0);
+                path.lineTo(0.06f * small, -0.38f * small);
+                path.lineTo(-0.17f * small, -0.38f * small);
+                path.lineTo(0.12f * small, -0.07f * small);
+                path.close();
+                canvas.drawPath(path, paint);
+            }
+        };
+        return shape;
+    }
+
     public static ShapeDrawable getRectangleDrawable(int color) {
         ShapeDrawable d = new ShapeDrawable(getRectangleShape());
         d.getPaint().setColor(color);
@@ -37,12 +103,12 @@ public class ShapeUtil {
             @Override
             public void draw(Canvas canvas, Paint paint) {
                 canvas.translate(w / 2, h / 2);
-                //Right Orientation
                 path.reset();
-                path.moveTo(-w / 2, -h / 2);
-                path.lineTo(w / 2, -h / 2);
-                path.lineTo(w / 2, h / 2);
-                path.lineTo(-w / 2, h / 2);
+                float small = w < h ? w : h;
+                path.moveTo(-small / 2, -small / 2);
+                path.lineTo(small / 2, -small / 2);
+                path.lineTo(small / 2, small / 2);
+                path.lineTo(-small / 2, small / 2);
                 path.close();
                 canvas.drawPath(path, paint);
             }
@@ -51,15 +117,15 @@ public class ShapeUtil {
     }
 
     public static ShapeDrawable[] getRegTris(int color) {
-        ShapeDrawable triBlackUp = ShapeUtil.getRegularTriangleDrawable("up", color);
-        ShapeDrawable triBlackDown = ShapeUtil.getRegularTriangleDrawable("down", color);
-        ShapeDrawable triBlackLeft = ShapeUtil.getRegularTriangleDrawable("left", color);
-        ShapeDrawable triBlackRight = ShapeUtil.getRegularTriangleDrawable("right", color);
+        ShapeDrawable triUp = ShapeUtil.getRegularTriangleDrawable("up", color);
+        ShapeDrawable triDown = ShapeUtil.getRegularTriangleDrawable("down", color);
+        ShapeDrawable triLeft = ShapeUtil.getRegularTriangleDrawable("left", color);
+        ShapeDrawable triRight = ShapeUtil.getRegularTriangleDrawable("right", color);
         ShapeDrawable[] shapeDrawables = new ShapeDrawable[4];
-        shapeDrawables[0] = triBlackUp;
-        shapeDrawables[1] = triBlackDown;
-        shapeDrawables[2] = triBlackLeft;
-        shapeDrawables[3] = triBlackRight;
+        shapeDrawables[0] = triUp;
+        shapeDrawables[1] = triDown;
+        shapeDrawables[2] = triLeft;
+        shapeDrawables[3] = triRight;
         return shapeDrawables;
     }
 
@@ -99,10 +165,11 @@ public class ShapeUtil {
                 }
                 //Right Orientation
                 path.reset();
-                path.moveTo(-w / 2, -h / 2);
-                path.lineTo(w / 2, 0);
-                path.lineTo(-w / 2, h / 2);
-                path.lineTo(-w / 2, -h / 2);
+                float small = w < h ? w : h;
+                path.moveTo(-small / 2, -small / 2);
+                path.lineTo(small / 2, 0);
+                path.lineTo(-small / 2, small / 2);
+                path.lineTo(-small / 2, -small / 2);
                 path.close();
                 canvas.drawPath(path, paint);
             }
